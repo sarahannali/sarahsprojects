cf2eea251b0bfa0e2a29eee25d974ebe75891253
Sarah <asarahali00@gmail.com>
2020-10-02 19:32:39 -0500

    finish home page

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -2,13 +2,29 @@ import React from 'react';
 import classes from './Home.module.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
 import RetroImage from '../../Components/RetroImage/RetroImage';
 
 function Home(props) {
     return (
-        <div>
-            
+        <div className={classes.Home}>
+            <Row>
+                <RetroImage />
+                <h1>SARAH'S PROJECTS</h1>
+            </Row>
+            <Row className={`d-flex justify-content-center ${classes.links}`}>
+                <Col xs={12} sm={3} className='d-flex justify-content-center'>
+                    <button className={classes.homeButton}>About</button>
+                </Col>
+                <Col xs={12} sm={3} className='d-flex justify-content-center'>
+                    <button className={classes.homeButton}>Projects</button>
+                </Col>
+                <Col xs={12} sm={3} className='d-flex justify-content-center'>
+                    <button className={classes.homeButton}>Cheat Sheets</button>
+                </Col>
+                <Col xs={12} sm={3} className='d-flex justify-content-center'>
+                    <button className={classes.homeButton}>Etc.</button>
+                </Col>
+            </Row>
         </div>
     );
 }
